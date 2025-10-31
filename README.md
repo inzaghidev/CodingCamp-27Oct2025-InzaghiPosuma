1eb1cfe7a6c21dca922acfc7c366c12b9f366d12
inzaghidev <izzumiposhaf29@gmail.com>
2025-11-01 00:27:23 +0700

    Edited some Files in HTML, README, and JS

---
+++ README.md
@@ -0,0 +1,3 @@
+# HTML CSS Mini Projects
+
+Merupakan Mini Project Pembuatan Apliasi To-Do List Sederhana oleh RevoU Software Engineering (SE)
--- js/script.js
+++ js/script.js
@@ -1,11 +1,52 @@
 console.log("Hello World!");
 
-function validateForm() {}
+let todoList = [];
 
-function addTodo() {}
+function validateForm(todo, date) {
+  if (todo.trim() === "") {
+    alert("Task input cannot be empty!");
+    return false;
+  }
+  if (date === "") {
+    alert("Due date cannot be empty!");
+    return false;
+  }
+  return true;
+}
 
-function deleteTodo() {}
+function addTodo() {
+  let todoInput = document.getElementById("todoInput").value;
+  let dateInput = document.getElementById("todo-date").value;
+  if (!validateForm(todoInput, dateInput)) {
+    return;
+  }
+  console.log("Adding todo:", todoInput, "with due date:", dateInput);
+  // Logic to add todo
+  todoList.push({ task: todoInput, date: dateInput });
+  renderTodo();
+}
 
-function filterTodo() {}
+function deleteTodo() {
+  console.log("Deleting all todos");
+  // Logic to delete all todos
+  todoList = [];
+  renderTodo();
+}
 
-function renderTodo() {}
+function filterTodo() {
+  let filterDate = document.getElementById("filterDate").value;
+  let filteredTodos = todoList.filter((todo) => todo.date === filterDate);
+  renderFilteredTodos(filteredTodos);
+}
+
+function renderFilteredTodos(filteredTodos) {
+  let taskList = document.getElementById("taskList");
+  taskList.innerHTML = ""; // Clear existing tasks
+  filteredTodos.forEach((todo) => {
+    let newRow = taskList.insertRow();
+    let taskCell = newRow.insertCell(0);
+    let dateCell = newRow.insertCell(1);
+    taskCell.textContent = todo.task;
+    dateCell.textContent = todo.date;
+  });
+}
